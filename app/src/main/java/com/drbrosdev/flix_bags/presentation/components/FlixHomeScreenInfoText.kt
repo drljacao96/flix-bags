212cb55b86ca099670a67d3128c2609d3020fb99
nikolaDrljaca <devnikoladr@gmail.com>
2022-07-03 17:21:54 +0300

    Centered info text

--- app/src/main/java/com/drbrosdev/flix_bags/presentation/components/FlixHomeScreenInfoText.kt
+++ app/src/main/java/com/drbrosdev/flix_bags/presentation/components/FlixHomeScreenInfoText.kt
@@ -1,8 +1,6 @@
 package com.drbrosdev.flix_bags.presentation.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,14 +17,21 @@ fun FlixHomeScreenInfoText(
 ) {
     Column(
         modifier = Modifier
+            .width(IntrinsicSize.Max)
             .then(modifier)
     ) {
         Text(
             text = headerText,
             style = MaterialTheme.typography.h4.copy(color = MaterialTheme.colors.onSurface),
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(4.dp))
-        Text(text = text, style = MaterialTheme.typography.caption.copy(color = Color.Gray))
+        Text(
+            text = text,
+            style = MaterialTheme.typography.caption.copy(color = Color.Gray),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
     }
 }
